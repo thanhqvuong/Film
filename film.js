2e66ab0217a8f9c52d67c94925f8d1ca986bb5fc
thanhqvuong <otto8758@gmail.com>
2024-12-10 12:26:44 +0700

    mm

--- film.js
+++ film.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Lấy nút "Đặt Vé"
     const bookingButton = document.querySelector(".btn-booking");
 
+    // Kiểm tra nếu nút không tồn tại
     if (!bookingButton) {
         console.error("Không tìm thấy nút đặt vé.");
         return; // Kết thúc nếu không tìm thấy nút
@@ -10,6 +11,6 @@ document.addEventListener("DOMContentLoaded", function () {
     // Thêm sự kiện khi nhấn vào nút
     bookingButton.addEventListener("click", function () {
         // Chuyển hướng đến trang chọn rạp
-        window.location.href = "theater.html";  // Thay đổi URL này nếu cần
+        window.location.href = "theater.html"; // Thay đổi URL này nếu cần
     });
 });
